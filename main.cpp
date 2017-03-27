0d3e82324a0cdf1a2d687b0b3d792cf8232b7bcc
viktor-elovikov <viktor_elovikov@mail.ru>
2017-03-27 10:33:45 +0500

    new version 2.0

--- main.cpp
+++ main.cpp
@@ -3,19 +3,42 @@
 #include <conio.h>
 #include <stdio.h>
 #include <malloc.h>
+
+
+/*
 typedef int T;
 typedef struct Stack_tag {
     T *data;
     size_t size;
     size_t top;
 } Stack_t;
+*/
 
+#define N 100
 #define INIT_SIZE 10
 #define MULTIPLIER 2
 #define STACK_OVERFLOW  -100
 #define STACK_UNDERFLOW -101
 #define OUT_OF_MEMORY   -102
 
+int prior(char c)
+{
+    switch (c)
+    {
+        case '(':
+            return 1;
+        case '+':
+            return 2;
+        case '-':
+            return 2;
+        case '*':
+            return 3;
+        case '/':
+            return 3;
+    }
+}
+
+/*
 Stack_t* createStack()
 {
     Stack_t *out = NULL;
@@ -34,6 +57,7 @@ Stack_t* createStack()
     out->top = 0;
     return out;
 }
+*/
 
 void deleteStack(Stack_t **stack)
 {
@@ -42,6 +66,7 @@ void deleteStack(Stack_t **stack)
     *stack = NULL;
 }
 
+/*
 void resize(Stack_t *stack)
 {
     stack->size *= MULTIPLIER;
@@ -51,6 +76,7 @@ void resize(Stack_t *stack)
         exit(STACK_OVERFLOW);
         }
 }
+
 void push(Stack_t *stack, T value)
 {
     if (stack->top >= stack->size)
@@ -60,6 +86,7 @@ void push(Stack_t *stack, T value)
     stack->data[stack->top] = value;
     stack->top++;
 }
+
 T pop(Stack_t *stack)
  {
     if (stack->top == 0)
@@ -82,21 +109,74 @@ void implode(Stack_t *stack)
     stack->size = stack->top;
     stack->data = (int*)realloc(stack->data, stack->size * sizeof(T));
 }
+
+*/
+
 int main()
 {
-    int i;
+    int i = 0, j = 0;
     Stack_t *s = createStack();
 
-    for (i = 0; i < 10; i++)
-        {
-        push(s, i);
-        }
-    for (i = 0; i < 10; i++)
-        {
-        printf("%d ", peek(s));
-        printf("%d ", pop(s));
-        }
+    char buf,str_in[N],str_out[N];
+
+    printf("plz enter ");
+    gets(str_in);
+
+    while (str_in[i])
+    {
+//ÅÑËÈ ÝËÅÌÅÍÒ ÑÒÐÎÊÈ ×ÈÑËÎ ÏÐÎÑÒÎ ÇÀÏÈÑÛÂÀÅÌ ÅÃÎ Â ÑÒÐÎÊÓ
+        if ( (str_in[i] >= '0') && (str_in[i] <= '9') )
+            {
+                str_out[j] = str_in[i];
+                ++j;
+            }
+//ÅÑÈË ÝËÅÌÅÍÒ ÑÒÐÎÊÈ ÎÒÊÐÛÂÀÞÙÀß ÑÊÎÁÊÀ ÇÀÍÎÑÈÌ ÅÅ Â ÑÒÝÊ
+        if ( str_in[i] == '(' ) push(s,str_in[i]);
+//ÅÑËÈ ÝËÅÌÅÍÒ ÑÒÐÎÊÈ ÇÀÊÐÛÂÀÞÙÀß ÑÊÎÁÊÀ ÇÀÏÈÑÛÂÀÅÌ ÂÑÅ ÝËÅÌÅÍÒÛ ÈÇ ÑÒÝÊ Â ÑÒÐÎÊÓ ÏÎÊÀ ÍÅ ÂÑÒÐÅÒÈÌ ÒÀÌ ÎÒÊÐÛÂÀÞÙÓÞ ÑÊÎÁÊÓ ÅÅ ÏÐÎÑÒÎ ÂÛÊÈÄÛÂÀÅÌ ÊÀÊ È ÇÀÊÐÛÂÀÞÙÓÞ
+        if ( str_in[i] == ')' )
+            while()
+                {
+                    buf = pop(s);
+                    if ( buf != '(' )
+                        {
+                            str_out[j]=buf;
+                            ++j;
+                        }
+                    else
+                        break;
+                }
+//ÅÑËÈ ÝËÅÌÅÍÒ ÑÒÐÎÊÈ ÝÒÎ ÎÏÅÐÀÒÎÐ + - * /
+        if ( (prior(str_in[i]) == 2) || (prior(str_in[i]) == 3) )
+            {
+                if ( s->top == 0 ) push(s,str_in[i]);
+                else
+                {
+                    while ()
+                    {
+                        if ( (prior(str_in[i]) > prior(s->top)) || (s->top == 0) )
+                            {
+                                push(s,str_in[i]);
+                                break;
+                            }
+                        else
+                            {
+                                buf = pop(s);
+                                str_out[j] = buf;
+                                ++j;
+                            }
+                    }
+                }
+            }
+        ++i;
+    }
+//ÂÑÅ ÎÑÒÀÂØÈÅÑß ÝËÅÌÅÍÒÛ ÑÒÝÊÀ ÇÀÏÈÑÛÂÀÅÌ Â ÑÒÐÎÊÓ
+    while ( s->top != 0 )
+    {
+        str_out[j]=pop(s);
+        ++j;
+    }
+
 
     deleteStack(&s);
-    _getch();
+    system("pause");
 }
